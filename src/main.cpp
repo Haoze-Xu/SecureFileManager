69125ed873172d206665fc3e49bad92131201338
Haoze-Xu <1624010188@qq.com>
2025-07-11 10:28:10 +0800

    Xzh

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     // 设置应用程序信息
     QApplication::setApplicationName("SecureFileManager");
     QApplication::setApplicationVersion("1.0");
-    QApplication::setOrganizationName("DeepSeek");
+    QApplication::setOrganizationName("XuZihao");
     
     // 创建并显示主窗口
     MainWindow w;
